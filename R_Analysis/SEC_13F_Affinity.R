7125f8faf55985809b18a154895327411ad0760a
DerrickStuckey <derrickstuckey@gmail.com>
2015-04-05 16:27:52 -0400

    added capability to set minimum ownership threshold

--- R_Analysis/SEC_13F_Affinity.R
+++ R_Analysis/SEC_13F_Affinity.R
@@ -3,8 +3,16 @@ library(arules)
 
 fund_holdings <- read.csv("~/Desktop/GW/Data Mining/SEC-13F/fund_holdings_v2.csv")
 
-# With no minimum threshold for security ownership: 
-fund_holdings <- fund_holdings[,1:2]
+# Set minimum threshold for security ownership:
+min_proportion <- 0.01
+fund_split_values <- split(x=fund_holdings[,"Value"],f=fund_holdings$Fund)
+fund_sums <- lapply(fund_split_values, sum)
+fund_sums_unsplit <- unsplit(fund_sums,f=fund_holdings$Fund)
+fund_holdings_2 <- data.frame(fund_holdings,"Fund_Value"=fund_sums_unsplit)
+fund_holdings_trimmed <- fund_holdings_2[fund_holdings_2$Value > fund_holdings_2$Fund_Value*min_proportion,]
+
+# rename trimmed dataframe back to original
+#fund_holdings <- fund_holdings_trimmed
 
 nrow(fund_holdings)
 
@@ -17,6 +25,8 @@ levels(fund_holdings$Security)
 fund_split <- split(x=fund_holdings[,"Security"],f=fund_holdings$Fund)
 fund_split <- lapply(fund_split,unique)
 
+#fund_split_2 <- split(x=c(fund_holdings[,"Security"],fund_holdings[,"Value"]),f=fund_holdings$Fund)
+
 #transform to transaction format
 transact <- as(fund_split,"transactions")
 
@@ -26,8 +36,8 @@ itemFrequencyPlot(transact,topN=20)
 #itemFrequencyPlot(transact,topN=10,horiz=TRUE)
 
 #only look at subsets of size 2 or less
-fund_rules <- apriori(transact,parameter=list(support=.10,confidence=.75,maxlen=2)) 
+fund_rules <- apriori(transact,parameter=list(support=.05,confidence=.75,maxlen=2)) 
 
 #view the rules
 inspect(fund_rules)
-inspect(subset(fund_rules, subset=lift > 5)) 
+inspect(subset(fund_rules, subset=lift > 10)) 
