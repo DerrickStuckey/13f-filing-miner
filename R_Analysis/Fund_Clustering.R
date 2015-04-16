4f709d756ecbbb575173ea5460bc8c63362b6439
DerrickStuckey <derrickstuckey@gmail.com>
2015-04-16 14:44:50 -0400

    k-means clustering attempt

--- R_Analysis/Fund_Clustering.R
+++ R_Analysis/Fund_Clustering.R
@@ -13,30 +13,52 @@ fund_sums_unsplit <- unsplit(fund_sums,f=fund_holdings$Fund)
 # add Fund_Value column to dataframe
 fund_holdings <- data.frame(fund_holdings,"Fund_Value"=fund_sums_unsplit)
 
+# calculate relative value for each holding
+fund_holdings$Rel_Value <- fund_holdings$Value / fund_holdings$Fund_Value
+#hist(fund_holdings_trimmed$Rel_Value,breaks=50,main="Relative Holding Size (all funds)")
+# Rel_Value appears exponentially distributed - maybe log-transforming will help?
+fund_holdings$Log_Rel_Value <- log(fund_holdings$Rel_Value)
+
 # Set minimum threshold for security ownership:
 min_proportion <- 0.01
-fund_holdings_trimmed <- fund_holdings[fund_holdings$Value > fund_holdings$Fund_Value*min_proportion,]
+fund_holdings_trimmed <- fund_holdings[fund_holdings$Rel_Value > min_proportion,]
 
 # ***try instead removing securities not held by very many funds***
 
 # reset factor levels for Security field 
 fund_holdings_trimmed$Security <- as.factor(as.character(fund_holdings_trimmed$Security))
 
-# calculate relative value for each holding
-fund_holdings_trimmed$Rel_Value <- fund_holdings_trimmed$Value / fund_holdings_trimmed$Fund_Value
-#hist(fund_holdings_trimmed$Rel_Value,breaks=50,main="Relative Holding Size (all funds)")
-# Rel_Value appears exponentially distributed - maybe log-transforming will help?
-fund_holdings_trimmed$Log_Rel_Value <- log(fund_holdings_trimmed$Rel_Value)
-
 # reshape data frame to "wide" format (Fund x Security matrix)
 library(reshape2)
 #funds_securities_values <- fund_holdings_trimmed[,-c(3,4,5)]
-funds_securities_values <- data.frame("Fund"=fund_holdings_trimmed$Fund,
-                                      "Security"=fund_holdings_trimmed$Security,
-                                      "Rel_Value"=fund_holdings_trimmed$Rel_Value)
-funds_wide <- dcast(funds_securities_values, Fund ~ Security, value.var="Rel_Value",fun.aggregate=sum,fill=0)
+funds_securities_values <- data.frame("Fund"=fund_holdings$Fund,
+                                      "Security"=fund_holdings$Security,
+                                      "Rel_Value"=fund_holdings$Rel_Value)
+funds_wide <- dcast(funds_securities_values, Fund ~ Security, value.var="Rel_Value",fun.aggregate=length,fill=0)
 row.names(funds_wide) <- funds_wide$Fund
 funds_wide <- funds_wide[,-1]
 
-#convert to a matrix
-funds_securities_matrix <- as.matrix(funds_wide)
+# remove securities only held by a few funds (sparse columns)
+security_counts <- lapply(funds_wide, sum)
+dense_securities_list <- security_counts > 10 #arbitrary number, gets to ~2000 securities
+#summary(dense_securities_list)
+dense_securities_df <- funds_wide[,dense_securities_list]
+
+# convert to a matrix
+funds_securities_matrix <- as.matrix(dense_securities_df)
+
+# try k-means clustering
+k <- 5
+k_means_fit <- kmeans(funds_securities_matrix, k) # 5 cluster solution
+clustered_funds <- data.frame("cluster"=k_means_fit$cluster, dense_securities_df)
+rsq <- k_means_fit$betweenss / k_means_fit$totss
+rsq
+
+# try k-modes clustering
+library(klaR)
+
+# takes quite a while:
+k_modes_fit <- kmodes(funds_securities_matrix, k)
+
+# hierarchical clustering
+
