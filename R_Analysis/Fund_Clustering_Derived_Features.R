3f4ba669bf79579423f5467917e501865f220e11
DerrickStuckey <derrickstuckey@gmail.com>
2015-04-28 13:00:31 -0400

    added plot of rsq vs k for clustering

--- R_Analysis/Fund_Clustering_Derived_Features.R
+++ R_Analysis/Fund_Clustering_Derived_Features.R
@@ -13,7 +13,26 @@ scaled_fund_stats$other_holdings <- log(scaled_fund_stats$other_holdings+1)
 # convert to a matrix
 funds_stats_matrix <- as.matrix(scaled_fund_stats)
 
-# try k-means clustering
+# # try with several values of k, get and plot r-sq vs k 
+# get_rsq <- function(k) {
+#   kmeans_fit <- kmeans(funds_stats_matrix, centers=k)
+#   rsq <- kmeans_fit$betweenss / kmeans_fit$totss
+#   return(rsq)
+# }
+# 
+# get_avg_rsq <- function(k) {
+#   trial_ks <- rep(k,5)
+#   trial_rsq <- lapply(trial_ks,get_rsq)
+#   avg_rsq <- mean(unlist(trial_rsq))
+# }
+# 
+# k_vals <- c(2,3,4,5,6,7,8,9)
+# rsq_vals <- lapply(k_vals, get_avg_rsq)
+# 
+# # plot rsq vs k
+# plot(k_vals, rsq_vals, xlab="k", ylab="R-squared", main="K-means Performance by # of Clusters", col="blue")
+
+# k-means clustering ith k=4
 k <- 4
 k_means_fit <- kmeans(funds_stats_matrix, k)
 clustered_funds <- data.frame("cluster"=k_means_fit$cluster, fund_derived_stats)
