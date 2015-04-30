acfc02605f31a5a4f8eb8c1b1f2da64c01380245
DerrickStuckey <derrickstuckey@gmail.com>
2015-04-30 11:15:56 -0400

    added size of largest holding as a feature

--- R_Analysis/Fund_Clustering_Derived_Features.R
+++ R_Analysis/Fund_Clustering_Derived_Features.R
@@ -8,31 +8,35 @@ scaled_fund_stats <- fund_derived_stats
 scaled_fund_stats$size <- log(scaled_fund_stats$size+1)
 scaled_fund_stats$common_holdings <- log(scaled_fund_stats$common_holdings+1)
 scaled_fund_stats$option_holdings <- log(scaled_fund_stats$option_holdings+1)
-scaled_fund_stats$other_holdings <- log(scaled_fund_stats$other_holdings+1)
+#scaled_fund_stats$other_holdings <- log(scaled_fund_stats$other_holdings+1)
+
+#remove other holdings column
+scaled_fund_stats <- scaled_fund_stats[,-3]
 
 # convert to a matrix
 funds_stats_matrix <- as.matrix(scaled_fund_stats)
+funds_stats_matrix <- scale(funds_stats_matrix)
 
-# # try with several values of k, get and plot r-sq vs k 
-# get_rsq <- function(k) {
-#   kmeans_fit <- kmeans(funds_stats_matrix, centers=k)
-#   rsq <- kmeans_fit$betweenss / kmeans_fit$totss
-#   return(rsq)
-# }
-# 
-# get_avg_rsq <- function(k) {
-#   trial_ks <- rep(k,5)
-#   trial_rsq <- lapply(trial_ks,get_rsq)
-#   avg_rsq <- mean(unlist(trial_rsq))
-# }
-# 
-# k_vals <- c(2,3,4,5,6,7,8,9)
-# rsq_vals <- lapply(k_vals, get_avg_rsq)
-# 
-# # plot rsq vs k
-# plot(k_vals, rsq_vals, xlab="k", ylab="R-squared", main="K-means Performance by # of Clusters", col="blue")
+# try with several values of k, get and plot r-sq vs k 
+get_rsq <- function(k) {
+  kmeans_fit <- kmeans(funds_stats_matrix, centers=k)
+  rsq <- kmeans_fit$betweenss / kmeans_fit$totss
+  return(rsq)
+}
 
-# k-means clustering ith k=4
+get_avg_rsq <- function(k) {
+  trial_ks <- rep(k,5)
+  trial_rsq <- lapply(trial_ks,get_rsq)
+  avg_rsq <- mean(unlist(trial_rsq))
+}
+
+k_vals <- c(2,3,4,5,6,7,8,9)
+rsq_vals <- lapply(k_vals, get_avg_rsq)
+
+# plot rsq vs k
+plot(k_vals, rsq_vals, xlab="k", ylab="R-squared", main="K-means Performance by # of Clusters", col="blue")
+
+# k-means clustering with k=4
 k <- 4
 k_means_fit <- kmeans(funds_stats_matrix, k)
 clustered_funds <- data.frame("cluster"=k_means_fit$cluster, fund_derived_stats)
@@ -45,28 +49,42 @@ plot(scaled_fund_stats$common_holdings, scaled_fund_stats$size,
      xlab="log(# holdings)",ylab="log(fund size)",
      main="Fund Size vs. Number of Common Holdings",
      col=k_means_fit$cluster)
-legend("topleft", legend = paste("Group", 1:4), pch=1, col=1:4)
+legend("topleft", legend = paste(1:4), pch=1, col=1:4)
 
 # plot # option holdings vs # common holdings
 plot(scaled_fund_stats$common_holdings, scaled_fund_stats$option_holdings,
      xlab="log(# common holdings)",ylab="log(# option holdings)",
      main="# of Option Holdings vs. # of Common Holdings",
      col=k_means_fit$cluster)
-legend("topleft", legend = paste("Group", 1:4), pch=1, col=1:4)
+legend("topleft", legend = paste(1:4), pch=1, col=1:4)
 
-# plot # other holdings vs # (common) holdings
-plot(scaled_fund_stats$common_holdings, scaled_fund_stats$other_holdings,
-     xlab="log(# common holdings)",ylab="log(# other holdings)",
-     main="# of Other Holdings vs. # of Common Holdings",
+# plot largest holdings vs # (common) holdings
+plot(scaled_fund_stats$common_holdings, scaled_fund_stats$largest_holding_prop,
+     xlab="log(# common holdings)",ylab="size of largest holding",
+     main="Size of Largest Holding vs. # of Common Holdings",
      col=k_means_fit$cluster)
-legend("topleft", legend = paste("Group", 1:4), pch=1, col=1:4)
+legend("topright", legend = paste(1:4), pch=1, col=1:4)
 
-# plot # option holdings vs # other holdings
-plot(scaled_fund_stats$other_holdings, scaled_fund_stats$option_holdings,
-     xlab="log(# other holdings)",ylab="log(# option holdings)",
-     main="# of Option Holdings vs. # of Other Holdings",
+# plot # option holdings vs largest holding
+plot(scaled_fund_stats$option_holdings, scaled_fund_stats$largest_holding_prop, 
+     xlab="log(# option holdings)",ylab="size of largest holding",
+     main="Size of Largest Holding vs. # of Option Holdings",
      col=k_means_fit$cluster)
-legend("topleft", legend = paste("Group", 1:4), pch=1, col=1:4)
+legend("topright", legend = paste(1:4), pch=1, col=1:4)
+
+# plot # option holdings vs largest holding
+plot(scaled_fund_stats$largest_holding_prop, scaled_fund_stats$option_holdings, 
+     xlab="size of largest holding",ylab="log(# option holdings)",
+     main="# of Option Holdings vs. Size of Largest Holding",
+     col=k_means_fit$cluster)
+legend("topright", legend = paste(1:4), pch=1, col=1:4)
+
+# plot size of largest holding vs fund size
+plot(scaled_fund_stats$size, scaled_fund_stats$largest_holding_prop, 
+     xlab="log(fund size)",ylab="size of largest holding",
+     main="Size of Largest Holding vs. Fund Size",
+     col=k_means_fit$cluster)
+legend("bottomleft", legend = paste(1:4), pch=1, col=1:4)
 
 group_1_funds <- names(k_means_fit$cluster[k_means_fit$cluster==1])
 group_1_derived_stats <- fund_derived_stats[k_means_fit$cluster==1,]
--- R_Analysis/Fund_Derived_Stats.R
+++ R_Analysis/Fund_Derived_Stats.R
@@ -7,8 +7,13 @@ fund_holdings <- read.csv("./fund_holdings_best.csv")
 # calculate total value of securities held for each fund
 fund_split_values <- split(x=fund_holdings[,"Value"],f=fund_holdings$Fund)
 fund_sums <- lapply(fund_split_values, sum)
-# log_fund_sums <- log(unlist(fund_sums)+1) #smoothing for log function
-#hist(unlist(log_fund_sums))
+#log_fund_sums <- log(unlist(fund_sums)+1) #smoothing for log function
+#hist(unlist(log_fund_sums), main="Log-transformed Fund Size", xlab="Log(Fund Size)")
+
+# calculate largest holding as a percentage of assets for each fund
+largest_holdings <- lapply(fund_split_values, max)
+largest_holdings_pct <- unlist(largest_holdings) / unlist(fund_sums)
+largest_holdings_pct[is.nan(largest_holdings_pct)] <- 1
 
 # calculate total # of holdings for each fund
 #fund_num_holdings <- lapply(fund_split_values, length)
@@ -30,10 +35,13 @@ fund_other_holdings <- lapply(fund_split_positions,
                                  sum(x=="other")
                                })
 
-# take log value of each count (w/ add-one smoothing)
-# log_common_holdings <- log(unlist(fund_common_holdings)+1)
-# log_option_holdings <- log(unlist(fund_option_holdings)+1)
-# log_other_holdings <- log(unlist(fund_other_holdings)+1)
+# Plot log-transformed histograms
+#log_common_holdings <- log(unlist(fund_common_holdings)+1)
+#log_option_holdings <- log(unlist(fund_option_holdings)+1)
+#log_other_holdings <- log(unlist(fund_other_holdings)+1)
+#hist(log_common_holdings, main="Log-transformed # of Common Holdings", xlab="Log(# Common Holdings)")
+#hist(log_option_holdings, breaks=20, main="Log-transformed # of Option Holdings", xlab="Log(# Option Holdings)")
+#hist(log_other_holdings, main="Log-transformed # of Other Holdings", xlab="Log(# Other Holdings)")
 
 # calculate concentration of holdings for each fund
 #...
@@ -42,6 +50,7 @@ fund_other_holdings <- lapply(fund_split_positions,
 fund_derived_stats <- data.frame("size"=unlist(fund_sums),
                                  "common_holdings"=unlist(fund_common_holdings),
                                  "other_holdings"=unlist(fund_other_holdings),
-                                 "option_holdings"=unlist(fund_option_holdings))
+                                 "option_holdings"=unlist(fund_option_holdings),
+                                 "largest_holding_prop"=largest_holdings_pct)
 
 write.csv(fund_derived_stats, "fund_derived_stats.csv", row.names=TRUE)
